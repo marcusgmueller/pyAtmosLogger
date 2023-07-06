f44dc51c5567046016c3a7d26570bf4a98de2349
Marcus Müller <22216795+marcusgmueller@users.noreply.github.com>
2023-07-06 13:20:28 +0200

    Update __init__.py

--- pyAtmosLogger/__init__.py
+++ pyAtmosLogger/__init__.py
@@ -1,4 +1,4 @@
 from . import pyAtmosLogger
 from .utils import utils
-from .instruments import ott_parsivel2_default
+from .instruments import ott_parsivel2_actris
 from .instruments import pyAtmosLogger_dummy_1
